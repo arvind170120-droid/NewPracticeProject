bd3d68b7e9c38d2b94074cb9e32e871317e56325
arvind170120-droid <arvind170120@gmail.com>
2026-02-20 18:23:44 +0530

    new 2nd line

--- src/test/java/Tasks/ClassByNandan.java
+++ src/test/java/Tasks/ClassByNandan.java
@@ -2,11 +2,10 @@ package Tasks;
 
 public class ClassByNandan {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("hey arvind");
 		
-		System.out.println("2nd line from b1");
+		System.out.println("new 2nd line");
 
 	}
 
